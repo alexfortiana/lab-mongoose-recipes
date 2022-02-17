02ede4c1d39f12a491a8ba81546ed1efa098753e
Chuanjo <chuanjo@hotmail.com>
2022-02-17 18:59:28 +0100

    Async await done

--- index-async-await.js
+++ index-async-await.js
@@ -8,49 +8,25 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
+
+//! COMPROBAR QUE ESTA TODO CORRECTO PORQUE NO HEMOS QUERIDO MODIFICAR 
+//! LA BASE DE DATOS CON EL THEN CATCH.
+
 mongoose
   .connect(MONGODB_URI)
-  // .then(x => {
-  //   console.log(`Connected to the database: "${x.connection.name}"`);
-  //   // Before adding any recipes to the database, let's remove all existing ones
-  //   return Recipe.deleteMany()
-  // })
-  // .then((response) => {
-  //   return Recipe.create({
-  //     title: "Huevo Duro"  
-  //   })
-
-  // })
-  // .then((response) => {
-  //   console.log("receta agregada")
-    
-  //   return Recipe.insertMany(data)
-  // })
-  // .then((response) => {
-  //   console.log("todas las recetas añadidas")
-  //   return Recipe.find(  {}, "title" )
-  // })
-  // .then((response) => {
-  //   console.log(response)
-  //   return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new: true})
-  // })
-  // .then((response) => {
-  //   console.log("Respuesta actualizada", response)
-
-  //   return Recipe.deleteOne({title: "Carrot Cake"})
-  // })
-  // .then((response) => {
-  //   console.log("receta borrada", response)
-  //   mongoose.disconnect();
-  // })
-  // .then((response) => {
-  //   console.log("esta desconectado")
-  // })
-  // .catch(error => {
-  //   console.error('Error connecting to the database', error);
-  // });
-
-
-  
-
-
+const obtainData = async() => {
+  try {
+    const response1 = await Recipe.deleteMany()
+    const response2 = await Recipe.create({
+      title: "Huevo Duro"
+    })
+    const response3 = await Recipe.insertMany(data)
+    const response4 = await Recipe.find({},"title" )
+    const response5 = await Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new: true})
+    const response6 = await Recipe.deleteOne({title: "Carrot Cake"})
+    const response7 = await mongoose.disconnect();
+  }
+  catch(err) {
+    console.log(err)
+  }
+}
